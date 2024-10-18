9daa84c4a305469efb33764768a059a765c265b0
maowu5 <111669347+maowu5@users.noreply.github.com>
2024-10-18 16:30:27 +1000

    Update login.php

--- src/login.php
+++ src/login.php
@@ -1,5 +1,5 @@
 <?php
-session_start();  // 启动会话
+session_start();
 
 include 'db_connection.php';
 
@@ -7,7 +7,6 @@ $data = json_decode(file_get_contents('php://input'), true);
 $username = $data['username'];
 $password = $data['password'];
 
-// 检查用户名和密码是否正确
 $sql = "SELECT * FROM users WHERE username = :username";
 $stmt = $pdo->prepare($sql);
 $stmt->bindParam(':username', $username);
@@ -15,10 +14,9 @@ $stmt->execute();
 $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
 if ($user && password_verify($password, $user['password_hash'])) {
-    // 登录成功，保存会话状态
     $_SESSION['loggedin'] = true;
     $_SESSION['username'] = $username;
-    $_SESSION['balance'] = $user['balance'];  // 保存用户的余额
+    $_SESSION['balance'] = $user['balance'];
     $_SESSION['user_id'] = $user['user_id']; 
     
     echo json_encode(['success' => true, 'username' => $username]);
