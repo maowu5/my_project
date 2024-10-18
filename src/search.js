1d3a7a119f274f123e415af245209fdb70bac250
maowu5 <111669347+maowu5@users.noreply.github.com>
2024-10-18 16:33:23 +1000

    Update search.js

--- src/search.js
+++ src/search.js
@@ -2,7 +2,6 @@ let isLoggedIn = false;
 let username = "";
 let balance = 0; 
 window.addEventListener('DOMContentLoaded', function() {
-    // 发起请求检查会话状态
     fetch('/check_login.php', {
         method: 'POST',
         headers: {
@@ -22,43 +21,40 @@ window.addEventListener('DOMContentLoaded', function() {
 
 document.addEventListener("DOMContentLoaded", function() {
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'search.php', true);  // 初次加载时，不带任何查询参数
+    xhr.open('GET', 'search.php', true);
 
     xhr.onload = function() {
         if (xhr.status === 200) {
-            // 更新 characters-grid 区域的内容
             document.getElementById('characters-grid').innerHTML = xhr.responseText;
         } else {
-            console.error('Error: ' + xhr.status);  // 打印错误信息到控制台
+            console.error('Error: ' + xhr.status);
         }
     };
 
     xhr.onerror = function() {
-        console.error('Request failed.');  // 处理请求错误
+        console.error('Request failed.');
     };
 
-    xhr.send();  // 发送请求
+    xhr.send();
 });
-// 获取搜索输入框
-document.getElementById('query').addEventListener('input', function() {
-    const query = document.getElementById('query').value;  // 获取输入值
 
-    // 创建一个新的 AJAX 请求
+document.getElementById('query').addEventListener('input', function() {
+    const query = document.getElementById('query').value;
+
     const xhr = new XMLHttpRequest();
-    xhr.open('GET', 'search.php?query=' + encodeURIComponent(query), true);  // 使用 GET 请求传递搜索词
+    xhr.open('GET', 'search.php?query=' + encodeURIComponent(query), true);
 
     xhr.onload = function() {
         if (xhr.status === 200) {
-            // 成功返回后，更新 characters-grid 区域的内容
             document.getElementById('characters-grid').innerHTML = xhr.responseText;
         } else {
-            console.error('Error: ' + xhr.status);  // 打印错误信息到控制台
+            console.error('Error: ' + xhr.status);
         }
     };
 
     xhr.onerror = function() {
-        console.error('Request failed.');  // 处理请求错误
+        console.error('Request failed.');
     };
 
-    xhr.send();  // 发送请求
+    xhr.send();
 });
