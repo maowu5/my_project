18b6e628f8bf6fa69c105cf87bf30d1ae576f483
maowu5 <111669347+maowu5@users.noreply.github.com>
2024-10-18 16:30:39 +1000

    Update logout.php

--- src/logout.php
+++ src/logout.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-session_unset();  // 清除所有会话变量
-session_destroy();  // 销毁会话
-header("Location: login.html");  // 重定向到登录页面
+session_unset();
+session_destroy();
+header("Location: login.html");
 exit;
 ?>
